b30a5290370ec0463f6670a9d98d5706cf7136be
feraistanti <fistanti551@gmail.com>
2025-12-18 22:43:36 +0700

    Menambahkan data class DataSiswa sebagai model data siswa

--- app/src/main/java/com/example/questapi_083/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_083/modeldata/DataSiswa.kt
@@ -10,33 +10,4 @@ data class DataSiswa (
     val telpon: String
 )
 
-data class UIStateSiswa(
-    val detailSiswa: DetailSiswa = DetailSiswa(),
-    val isEntryValid: Boolean = false
-)
-
-data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telpon: String = "",
-)
-
-fun DetailSiswa.toDataSiswa() : DataSiswa = DataSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon
-)
-
-fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
-
-fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon
 )
