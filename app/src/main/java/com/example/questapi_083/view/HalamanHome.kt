05ebad5b12f3085a79cb288bde80b5850270787c
feraistanti <fistanti551@gmail.com>
2025-12-25 23:01:49 +0700

    Halaman Home menampilkan daftar siswa, tombol tambah data, dan navigasi ke detail/update.

--- app/src/main/java/com/example/questapi_083/view/HalamanHome.kt
+++ app/src/main/java/com/example/questapi_083/view/HalamanHome.kt
@@ -34,24 +34,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.questapi_083.uicontroller.route.DestinasiHome
-import com.example.questapi_083.viewmodel.HomeViewModel
-import com.example.questapi_083.viewmodel.provider.PenyediaViewModel
 import com.example.questapi_083.R
 import com.example.questapi_083.modeldata.DataSiswa
+import com.example.questapi_083.uicontroller.route.DestinasiHome
+import com.example.questapi_083.viewmodel.HomeViewModel
 import com.example.questapi_083.viewmodel.StatusUiSiswa
-
-
+import com.example.questapi_083.viewmodel.provider.PenyediaViewModel
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    //edit 1.1 : tambahkan parameter navigateToItemEntry
     navigateToItemEntry: () -> Unit,
-    //edit 2.4 : tambahkan parameter navigateToItemUpdate
-    navigateToItemUpdate:(Int) -> Unit,
-    modifier: Modifier = Modifier,
+    navigateToItemUpdate: () -> Unit,
+    modifier: Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
@@ -61,12 +57,11 @@ fun HomeScreen(
             SiswaTopAppBar(
                 title = stringResource(DestinasiHome.titleRes),
                 canNavigateBack = false,
-                scrollBehavior = scrollBehavior
+                scrollBehavior= scrollBehavior
             )
         },
         floatingActionButton = {
             FloatingActionButton(
-                //edit 1.2 : event onClick
                 onClick = navigateToItemEntry,
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
@@ -77,10 +72,11 @@ fun HomeScreen(
                 )
             }
         },
-    ) { innerPadding ->
+    ) {
+            innerPadding ->
         HomeBody(
             statusUiSiswa = viewModel.listSiswa,
-            onSiswaClick = navigateToItemUpdate,
+            onSiswaClick = {navigateToItemUpdate},
             retryAction = viewModel::loadSiswa,
             modifier = modifier
                 .padding(innerPadding)
@@ -88,108 +84,4 @@ fun HomeScreen(
         )
     }
 }
-@Composable
-fun HomeBody(
-    statusUiSiswa: StatusUiSiswa,
-    //edit 2.3 tambahkan parameter onSiswaClick
-    onSiswaClick: (Int) -> Unit,
-    retryAction: () -> Unit,
-    modifier: Modifier = Modifier
-){
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ){
-        when(statusUiSiswa){
-            is StatusUiSiswa.Loading -> LoadingScreen()
-            //edit 2.5 : tambahkan event onSiswaClick
-            is StatusUiSiswa.Success -> DaftarSiswa(itemSiswa = statusUiSiswa.Siswa,
-                onSiswaClick = {onSiswaClick(it.id)} )
-            is StatusUiSiswa.Error -> ErrorScreen(
-                retryAction,
-                modifier = modifier.fillMaxSize()
-            )
-        }
-    }
-}
-@Composable
-fun LoadingScreen(modifier: Modifier = Modifier) {
-    Image(
-        modifier = modifier.size(200.dp),
-        painter = painterResource(R.drawable.loading_img),
-        contentDescription = stringResource(R.string.loading)
-    )
-}
 
-@Composable
-fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = stringResource(R.string.gagal), modifier = Modifier.padding(16.dp))
-        Button(onClick = retryAction) {
-            Text(stringResource(R.string.retry))
-        }
-    }
-}
-@Composable
-fun DaftarSiswa(
-    itemSiswa : List<DataSiswa>,
-    //edit 2.1 : tambahkab parameter onSiswaClick
-    onSiswaClick: (DataSiswa) -> Unit,
-    modifier: Modifier=Modifier
-){
-    LazyColumn(modifier = Modifier){
-        items(items = itemSiswa, key = {it.id}){
-                person ->
-            ItemSiswa(
-                siswa = person,
-                modifier = Modifier
-                    .padding(dimensionResource(id = R.dimen.padding_small))
-                    //edit 2.2 jadikan itemsiswa menjadi clickable()
-                    .clickable { onSiswaClick(person) }
-            )
-        }
-    }
-}
-
-@Composable
-fun ItemSiswa(
-    siswa: DataSiswa,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier,
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(
-                id = R.dimen.padding_small))
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    text = siswa.nama,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-                Spacer(Modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Default.Phone,
-                    contentDescription = null,
-                )
-                Text(
-                    text = siswa.telpon,
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
-            Text(
-                text = siswa.alamat,
-                style = MaterialTheme.typography.titleMedium
-            )
-        }
-    }
-}
