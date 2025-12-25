4dae1da7e9cffeefe99f4aa4ae226e68e899fe76
feraistanti <fistanti551@gmail.com>
2025-12-25 22:58:30 +0700

    ViewModel untuk edit data siswa, memuat data awal dan validasi input.

--- app/src/main/java/com/example/questapi_083/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_083/viewmodel/EditViewModel.kt
@@ -1,2 +1,49 @@
 package com.example.questapi_083.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_083.modeldata.DetailSiswa
+import com.example.questapi_083.modeldata.UIStateSiswa
+import com.example.questapi_083.modeldata.toDataSiswa
+import com.example.questapi_083.modeldata.toUIStateSiswa
+import com.example.questapi_083.repositori.RepositoryDataSiswa
+import com.example.questapi_083.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa =
+                repositoryDataSiswa.getSatuSiswa(idSiswa).toUIStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
