fa1f861a179b58904c837896f884f52ac0bb86b4
feraistanti <fistanti551@gmail.com>
2025-12-25 22:55:44 +0700

    Objek destinasi navigasi Edit, pakai parameter id siswa.

--- app/src/main/java/com/example/questapi_083/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_083/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_083.uicontroller.route
 
+import com.example.questapi_083.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "edit_siswa"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
