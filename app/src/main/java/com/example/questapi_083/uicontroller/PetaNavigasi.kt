f2af95080fa64516ede9052b1d12f3fc24af5235
feraistanti <fistanti551@gmail.com>
2025-12-25 23:07:02 +0700

    Setup Navigation Compose untuk Home, Entry, Detail, dan Edit siswa.

--- app/src/main/java/com/example/questapi_083/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_083/uicontroller/PetaNavigasi.kt
@@ -1,16 +1,22 @@
 package com.example.questapi_083.uicontroller
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.questapi_083.uicontroller.route.DestinasiDetail
 import com.example.questapi_083.uicontroller.route.DestinasiEntry
 import com.example.questapi_083.uicontroller.route.DestinasiHome
 import com.example.questapi_083.view.EntrySiswaScreen
 import com.example.questapi_083.view.HomeScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questapi_083.view.DetailSiswaScreen
+import com.example.questapi_083.modeldata.DetailSiswa
+import com.example.questapi_083.uicontroller.route.DestinasiEdit
+import com.example.questapi_083.view.EditSiswaScreen
 
 @Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(),
@@ -39,6 +45,18 @@ fun HostNavigasi(
             EntrySiswaScreen(navigateBack = {
                 navController.navigate(DestinasiHome.route)
             })
+        }
+        composable ( DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArg){
+            type = NavType.IntType })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+        composable ( DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg){
+            type = NavType.IntType })
+        ){
+            EditSiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)},
+                onNavigateUp = {navController.navigateUp()})
         }
     }
 }
