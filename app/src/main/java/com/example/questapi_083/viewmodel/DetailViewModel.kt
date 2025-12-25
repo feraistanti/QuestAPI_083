36e18608d0a109053e65dc7e4618688c6047374f
feraistanti <fistanti551@gmail.com>
2025-12-25 22:57:01 +0700

    ViewModel untuk detail siswa, ambil data berdasarkan id dan kelola status UI (Loading, Success, Error).

--- app/src/main/java/com/example/questapi_083/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_083/viewmodel/DetailViewModel.kt
@@ -1,2 +1,36 @@
 package com.example.questapi_083.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_083.modeldata.DataSiswa
+import com.example.questapi_083.repositori.RepositoryDataSiswa
+import com.example.questapi_083.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriDataSiswa: RepositoryDataSiswa
+) : ViewModel(){
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
